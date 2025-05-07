8e45848e05a0376f9ef16ab60deb1488a06d4fe4
Abhinav <abhinavshinde47@gmail.com>
2025-05-07 19:28:13 +0530

    hello

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import'./App.css'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-3xl bg-slate-500'>Currency Change</h2>
+    </>
+  )
+}
+
+export default App
