a793e4d16352d05b6d91f7e84781b7dc39c6e533
AbhinavShinde25 <abhinavshinde47@gmail.com>
2025-06-08 23:46:05 +0530

    last

--- 04Prac-color/src/App.jsx
+++ 04Prac-color/src/App.jsx
@@ -9,12 +9,12 @@ function App() {
       <div className='w-full h-screen bg-slate-600'
        style={{backgroundColor:color}}>
 
-        <div className="mt-6">
-          <div className="bg-gray-600 rounded bottom-10">
-            <button className="button bg-red-700 rounded-md m-3 p-3" onClick={()=>setColor("red")}>RED</button>
-            <button className="button bg-orange-500 rounded-md m-3 p-3" onClick={()=>setColor("orange")}>orange</button>
-            <button className="button bg-green-700 rounded-md m-3 p-3" onClick={()=>setColor("green")}>green</button>
-            <button className="button bg-yellow-500 rounded-md m-3 p-3" onClick={()=>setColor("yellow")}>yellow</button>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className="button bg-red-700   outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={()=>setColor("red")}>RED</button>
+            <button className="button bg-orange-500  outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={()=>setColor("orange")}>orange</button>
+            <button className="button bg-green-700   outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={()=>setColor("green")}>green</button>
+            <button className="button bg-yellow-500  outline-none px-4 py-1 rounded-full text-white shadow-lg" onClick={()=>setColor("yellow")}>yellow</button>
          
           </div>
         </div>
--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -1,4 +1,5 @@
-import React ,{useId} from 'react'
+/* eslint-disable react/prop-types */
+import  {useId} from 'react'
 
 function InputBox({
     label,
@@ -9,47 +10,40 @@ function InputBox({
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
-    className ="",
-
-    
-    
+    className = "",
 }) {
-    const amountInputId = useId(); 
+   const amountInputId = useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor= {amountInputId} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
                     id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled ={amountDisable}
-                    value = {amount}
-                    onChange={(e) =>(
-                        onAmountChange && onAmountChange(Number(e.target.value))
-                    )}
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">{selectCurrency}</p>
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    disabled ={currencyDisable}
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-                    
+                    disabled={currencyDisable}
                 >
                     
-                    {currencyOptions.map((currency) => (
+                        {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
                             {currency}
                             </option>
-                    ))}
+                        ))}
                 
                 </select>
             </div>
